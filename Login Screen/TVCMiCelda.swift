22292b447de45c97df3a504b5e27a0b04395bbad
AlejandroRamirezCallado <alejandro.ramirez@live.u-tad.com>
2017-03-27 19:36:40 +0200

    Commit

---
+++ Login Screen/TVCMiCelda.swift
@@ -0,0 +1,27 @@
+//
+//  TVCMiCelda.swift
+//  Login Screen
+//
+//  Created by Alejandro Ramírez Callado on 27/3/17.
+//  Copyright © 2017 Borja Alonso-Majagranzas Mendicote. All rights reserved.
+//
+
+import UIKit
+
+class TVCMiCelda: UITableViewCell {
+
+    @IBOutlet var lblNombre:UILabel?
+    @IBOutlet var img:UIImageView?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Login Screen/VCItem3.swift
@@ -0,0 +1,35 @@
+//
+//  VCItem3.swift
+//  Login Screen
+//
+//  Created by Alejandro Ramírez Callado on 27/3/17.
+//  Copyright © 2017 Borja Alonso-Majagranzas Mendicote. All rights reserved.
+//
+
+import UIKit
+
+class VCItem3: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
